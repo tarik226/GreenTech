7b74c976a7e5872ee1676f9f762e411ce86c0b31
tarikk <tarikelyatim2006@gmail.com>
2026-02-05 19:50:28 +0100

    enum des categorie (unutilisable)

---
+++ app/CategorieEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+
+enum CategorieEnum: string
+{
+    case ELECTRONICS = 'electronics';
+    case CLOTHING = 'clothing';
+    case FOOD = 'food';
+    case BOOKS = 'books';
+}
