e0d1717282ff547d2c26a3c89c55db238bf4b507
tarikk <tarikelyatim2006@gmail.com>
2026-02-05 19:45:09 +0100

    restriction d acces pour l user

---
+++ app/Http/Middleware/AdminMidlleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMidlleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // dd(Auth::user()->role );
+        if (Auth::user()->role == 'client') {
+            # code...
+            abort(401,'access denied');
+        }
+        return $next($request);
+    }
+}
